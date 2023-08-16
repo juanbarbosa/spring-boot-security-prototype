cb2d59a0eefe4e8e21c9e061d04ea6f6ccaf235e
Juan Barbosa <juan.barbosa@empowerme.co>
2023-08-16 16:59:55 -0400

    removed tests, breaks mvn package command

--- src/main/java/com/gmail/juanbarbosa/custom/ThreadContextFilter.java
+++ src/main/java/com/gmail/juanbarbosa/custom/ThreadContextFilter.java
@@ -16,31 +16,9 @@ public class ThreadContextFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         storeDomainContext(request);
-        determineCookieSupport(request);
         
         filterChain.doFilter(request, response);
     }
-
-	private void determineCookieSupport(HttpServletRequest request) {
-		// Check if the cookie exists
-        Cookie[] cookies = request.getCookies();
-        boolean hasCookie = false;
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("cookie_test".equals(cookie.getName())) {
-                    hasCookie = true;
-                    break;
-                }
-            }
-        }
-
-        // Add logic to handle the absence of the cookie
-        if (!hasCookie) {
-        	// TODO set warning message or something
-            // You can set a warning message or redirect to a specific page
-            // response.sendRedirect("/cookie-warning");
-        }
-	}
 
 	private void storeDomainContext(HttpServletRequest request) {
 		String domain = request.getServerName();
